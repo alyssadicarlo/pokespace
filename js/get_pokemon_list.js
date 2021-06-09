a2dd6439d5deefe36b79d7a177fb1d347e7678a1
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-06-09 08:00:11 -0700

    added search bar on results page and moved all search input js into a different file

---
+++ js/get_pokemon_list.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const datalist = document.querySelector('datalist');
+    const searchInput = document.querySelector('#searchInput');
+    let pokemon = [];
+
+    searchInput.addEventListener('keyup', (event) => {
+        if (searchInput.value) {
+            datalist.id = "pokemonList";    		
+        } else {
+            datalist.id = "";
+        }
+    });
+
+    function fetchPokemon() {
+        fetch(
+            `https://pokeapi.co/api/v2/pokemon?offset=0&limit=1118`
+        ).then((response) => {
+            return response.json();
+        }).then((data) => {
+            addPokemonToList(data);
+        });
+    }
+
+    function addPokemonToList(data) {
+        const pokemonData = data.results;
+                
+        pokemonData.forEach((item) => {
+            pokemon.push(item.name);
+        });
+
+        updatePokemonSelectElement();
+    }
+
+    function updatePokemonSelectElement() {
+
+        pokemon.forEach((item) => {
+            const optionElement = document.createElement('option');
+            optionElement.value = item;
+            optionElement.innerText = item;
+
+            datalist.append(optionElement);
+        });
+    }
+
+    fetchPokemon();
+});
